06973d7e6270891bf80f91f9c71b1d003fd11624
Annie Wang <anniewang2023@gmail.com>
2025-03-31 12:54:40 -0400

    fix projects routing

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,14 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
   plugins: [react()],
+  base: "/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        fallback: "404.html", // we'll copy index.html to 404.html after build
+      },
+    },
+  },
 });
